ee13f4ec2e7c82ee1c4e2fcd925f8888fdeb6350
Emmanuel Coulombe <emmanuelcoulombe@civalgo.com>
2024-05-01 10:32:36 -0400

    fix lsp go to definition typescript + treesitter

--- lua/imrashb/plugins/mason_lsps.lua
+++ lua/imrashb/plugins/mason_lsps.lua
@@ -1,43 +1,46 @@
 require("mason").setup({
-	ui = {
-		icons = {
-			package_installed = "✓",
-			package_pending = "➜",
-			package_uninstalled = "✗",
-		},
-	},
+    ui = {
+        icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗",
+        },
+    },
 })
 
 local lspconfig = require("lspconfig")
 local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 require("mason-lspconfig").setup({
-	ensure_installed = { "lua_ls", "tsserver", "eslint" },
-	handlers = {
-		function(server)
-			lspconfig[server].setup({
-				capabilities = lsp_capabilities,
-			})
-		end,
-		["tsserver"] = function()
-			lspconfig.tsserver.setup({
-				capabilities = lsp_capabilities,
-				settings = {
-					completions = {
-						completeFunctionCalls = true,
-					},
-				},
-			})
-		end,
-	},
+    ensure_installed = { "lua_ls", "tsserver" },
+    handlers = {
+        function(server)
+            lspconfig[server].setup({
+                capabilities = lsp_capabilities,
+            })
+        end,
+        ["tsserver"] = function()
+            lspconfig.tsserver.setup({
+                capabilities = lsp_capabilities,
+                settings = {
+                    completions = {
+                        completeFunctionCalls = true,
+                    },
+                },
+                on_attach = function(client, bufnr)
+                    vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+                end,
+            })
+        end,
+    },
 })
 
 require("lspconfig").lua_ls.setup({
-	settings = {
-		Lua = {
-			diagnostics = {
-				globals = { "vim" },
-			},
-		},
-	},
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { "vim" },
+            },
+        },
+    },
 })
---
+++ lua/imrashb/plugins/treesitter.lua
@@ -0,0 +1,24 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
+
+		configs.setup({
+			ensure_installed = {
+				"c",
+				"lua",
+				"vim",
+				"vimdoc",
+				"query",
+				"javascript",
+				"html",
+				"typescript",
+			},
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+			auto_install = true,
+		})
+	end,
+}
