833b1c69050eeb9d1c9b364e3d932ab6adcac86f
ImRashB <45215136+imrashb@users.noreply.github.com>
2024-04-30 22:10:04 -0400

    ts/js lsp setup

--- lua/imrashb/plugins/mason_formatters.lua
+++ lua/imrashb/plugins/mason_formatters.lua
@@ -4,7 +4,6 @@ local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 null_ls.setup({
 	sources = {
 		null_ls.builtins.formatting.stylua,
-		null_ls.builtins.formatting.prettier,
 		require("none-ls.diagnostics.eslint_d"),
 		require("none-ls.formatting.eslint_d"),
 		-- null_ls.builtins.completion.spell,
--- lua/imrashb/plugins/mason_lsps.lua
+++ lua/imrashb/plugins/mason_lsps.lua
@@ -8,8 +8,28 @@ require("mason").setup({
 	},
 })
 
+local lspconfig = require("lspconfig")
+local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+
 require("mason-lspconfig").setup({
-	ensure_installed = { "lua_ls" },
+	ensure_installed = { "lua_ls", "tsserver", "eslint" },
+	handlers = {
+		function(server)
+			lspconfig[server].setup({
+				capabilities = lsp_capabilities,
+			})
+		end,
+		["tsserver"] = function()
+			lspconfig.tsserver.setup({
+				capabilities = lsp_capabilities,
+				settings = {
+					completions = {
+						completeFunctionCalls = true,
+					},
+				},
+			})
+		end,
+	},
 })
 
 require("lspconfig").lua_ls.setup({
