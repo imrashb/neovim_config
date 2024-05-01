a33f9cfead700a8e0625e00b2421d7b106223c8a
Emmanuel Coulombe <emmanuelcoulombe@civalgo.com>
2024-05-01 11:29:23 -0400

    show hidden files telescope

--- init.lua
+++ init.lua
@@ -1,14 +1,14 @@
 -- Lazy
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -21,6 +21,13 @@ require("gitblame").setup()
 
 require("colorizer").setup()
 require("gitsigns").setup()
+require("telescope").setup({
+    pickers = {
+        find_files = {
+            hidden = true,
+        },
+    },
+})
 
 -- LSP / LINTING / FORMATTING
 require("imrashb.plugins.mason_lsps")
