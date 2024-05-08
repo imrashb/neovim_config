3b4f3f07b8c9723f824570e19bb753b36b2a802b
Emmanuel Coulombe <emmanuelcoulombe@civalgo.com>
2024-05-08 11:25:49 -0400

    change lualine theme to tokyonight + better diff colors

--- init.lua
+++ init.lua
@@ -16,7 +16,37 @@ require("lazy").setup(require("imrashb.plugins"))
 
 require("imrashb.options")
 require("imrashb.keybinds")
-require("imrashb.plugins.evil_lualine")
+-- require("imrashb.plugins.evil_lualine")
+require("lualine").setup({
+	options = {
+		theme = "tokyonight",
+	},
+	sections = {
+		lualine_c = {
+			{
+				-- Lsp server name .
+				function()
+					local msg = "No Active Lsp"
+					local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
+					local clients = vim.lsp.get_active_clients()
+					if next(clients) == nil then
+						return msg
+					end
+					for _, client in ipairs(clients) do
+						local filetypes = client.config.filetypes
+						if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+							return client.name
+						end
+					end
+					return msg
+				end,
+				icon = " LSP:",
+				color = { fg = "#ffffff", gui = "bold" },
+			},
+		},
+	},
+})
+
 require("gitblame").setup()
 
 require("colorizer").setup()
@@ -37,3 +67,7 @@ require("imrashb.plugins.mason_lsps")
 require("imrashb.plugins.mason_formatters")
 
 vim.cmd.colorscheme("tokyonight-storm")
+
+-- Diff Colors
+vim.cmd([[:highlight DiffAdd guibg=#24603b]])
+vim.cmd([[:highlight DiffDelete guibg=#56283b]])
