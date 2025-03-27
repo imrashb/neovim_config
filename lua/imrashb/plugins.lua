e29930eb983b1a4d80d087532dcb43556c47367d
Emmanuel Coulombe <emmanuelcoulombe@civalgo.com>
2025-03-27 13:00:39 -0400

    show markdown correctly

--- lua/imrashb/plugins.lua
+++ lua/imrashb/plugins.lua
@@ -51,13 +51,8 @@ return {
 		end,
 	},
 	{
-		"iamcco/markdown-preview.nvim",
-		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-		build = "cd app && npm install",
-		init = function()
-			vim.g.mkdp_filetypes = { "markdown" }
-		end,
-		ft = { "markdown" },
+		"MeanderingProgrammer/render-markdown.nvim",
+		ft = { "markdown", "codecompanion" },
 	},
 	-- {
 	-- 	"nvim-neotest/neotest",
